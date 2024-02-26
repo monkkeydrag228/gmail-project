5aafca40f04a08700651c4656d98fdc10c6df579
monkkeydrag228 <mirjalilovodil@gmail.com>
2024-02-26 15:54:02 +0300

    first changing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-navbar :visible="isAuth"></the-navbar>
+  <div class="container with-nav">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheNavbar from './components/TheNavbar'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isAuth: true
+    }
+  },
+  methods: {
+    login() {
+      this.isAuth = true
+      if (this.$route.query.page) {
+        this.$router.push(this.$route.query.page)
+      } else {
+        this.$router.push('/dashboard')
+      }
+    },
+    logout() {
+      this.isAuth = false
+      this.$router.push({
+        path: '/login',
+        query: {
+          page: this.$route.path
+        }
+      })
+    }
+  },
+  components: {TheNavbar},
+  provide() {
+    return {
+      login: this.login,
+      logout: this.logout,
+      emails: [
+        {id: '1', theme: 'Купил себе PlayStation 5'},
+        {id: '2', theme: 'Выучил Vue Router'},
+        {id: '3', theme: 'Хочу изучить весь Vue'},
+        {id: '4', theme: 'А следующий блок про Vuex!'},
+        {id: '5', theme: 'А что там на счет Vue Hooks?'}
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
